4ebc3b92d5fa648944abc0b0e2f3d6518a9d1abf
Thomas Tridon <ttridon.pro@gmail.com>
2023-07-24 12:23:29 +0200

    - Mise à jour du Readme. - Mise en place de l'architecture. - Initialisation du projet (serveur API). - Création des routes pour les Listes.

---
+++ app/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+// dotenv est configuré pour aller chercher automatiquement un fichier .env à la racine du projet (si on change le nom ou l'emplacement du fichier, dotenv ne fonctionnera plus).
+require('dotenv').config(); // Equivalent à écrire const dotenv = require('dotenv'); dotenv.config();
+// Si je require un dossier, node va automatiquement chercher un fichier avec le nom index.js.
+const routers = require('./routers');
+
+const app = express();
+
+// Permet de récupérer le body (sous forme de FormData) dans les requêtes et de le rendre disponible via req.body.
+// app.use(express.urlencoded({ extended: true }));
+// Permet de récupérer le body (sous forme de JSON) dans les requêtes et de le rendre disponible via req.body.
+app.use(express.json());
+
+app.use(routers);
+
+app.listen(3000, () => {
+    console.log('Listening on: http://localhost:3000');
+})
