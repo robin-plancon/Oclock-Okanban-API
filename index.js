d12aaedce96839f3e8d207015f49dc147d07dea2
Robin <robin.plancon@outlook.com>
2023-07-27 15:26:32 +0200

    add cors rules

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+app.use(
+	cors({
+		origin: '*',
+	})
+);
 
 // Local imports
 const router = require('./app/router');
