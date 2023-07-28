a1ff503356dbe1eb5d1b70b677af9277a96ec21b
Thomas Tridon <ttridon.pro@gmail.com>
2023-07-28 12:10:30 +0200

    ajout de multer: gestion du formData

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require('cors');
 const sanitizer = require('sanitizer');
+const multer = require('multer');
 // dotenv est configuré pour aller chercher automatiquement un fichier .env à la racine du projet (si on change le nom ou l'emplacement du fichier, dotenv ne fonctionnera plus).
 require('dotenv').config(); // Equivalent à écrire const dotenv = require('dotenv'); dotenv.config();
 // Si je require un dossier, node va automatiquement chercher un fichier avec le nom index.js.
 const routers = require('./routers');
+
+const bodyParser = multer();
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,6 +21,9 @@ app.use(cors({
 // app.use(express.urlencoded({ extended: true }));
 // Permet de récupérer le body (sous forme de JSON) dans les requêtes et de le rendre disponible via req.body.
 app.use(express.json());
+// on utlise .none() pour dire qu'on attends pas de fichier, uniquement des inputs "classiques" !
+app.use(bodyParser.none());
+
 app.use((req, res, next) => {
     if (req.body) {
         for (let propName in req.body) {
